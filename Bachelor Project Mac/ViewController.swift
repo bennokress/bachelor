5fb3a43a1b218fe6862002d3db7bae9ad2ef1666
Benno Kress <bkress@it-economics.de>
2017-04-27 10:44:06 +0200

    Initial Commit

---
+++ Bachelor Project Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bachelor Project Mac
+//
+//  Created by Benno Kress on 27.04.17.
+//  Copyright © 2017 it-economics. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Bachelor Project MacTests/Bachelor_Project_MacTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bachelor_Project_MacTests.swift
+//  Bachelor Project MacTests
+//
+//  Created by Benno Kress on 27.04.17.
+//  Copyright © 2017 it-economics. All rights reserved.
+//
+
+import XCTest
+@testable import Bachelor_Project_Mac
+
+class Bachelor_Project_MacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
